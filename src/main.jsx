71931c68a2d54521f8bab4b05654ddc03e1ffcb2
Derutron <fidg_1@yahoo.com>
2023-12-23 17:42:41 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -26,21 +26,3 @@ createRoot.render(
   document.getElementById('root')
 );
 
-
-
-
-
-// import React from 'react';
-// import ReactDOM, { createRoot } from 'react-dom';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import App from './App.jsx';
-// import './css/styles.css';
-
-// // Use createRoot method
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <Router>
-//       <App />
-//     </Router>
-//   </React.StrictMode>
-// );
